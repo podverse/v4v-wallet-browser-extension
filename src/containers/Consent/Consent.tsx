c911821c1c07045d5723f13bdc210ba541ecfc49
Mitch Downey <mitchldowney@gmail.com>
2022-01-02 19:08:22 -0600

    Change name to v4v-wallet-browser-extension

--- src/containers/Consent/Consent.tsx
+++ src/containers/Consent/Consent.tsx
@@ -30,7 +30,7 @@ export const Consent = ({ hideContainer, setCurrentPage }: Props) => {
       <p>Only use the V4V Wallet for sending small amounts that you are willing to lose if something should go wrong.</p>
       <p>Sending Bitcoin over the Lightning Network may include a network fee. The V4V Wallet does not control the network fee, and it is automatically added to your transaction amount.</p>
       <p>All code for this extension is provided under a free and open source license.</p>
-      <p><Link target='_blank' text='Code Repository' url='https://github.com/podverse/v4v-wallet-chrome-extension' /></p>
+      <p><Link target='_blank' text='Code Repository' url='https://github.com/podverse/v4v-wallet-browser-extension' /></p>
       <div className='checkbox-wrapper'>
         <Checkbox
           checked={hasAccepted}
